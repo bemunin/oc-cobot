de31d2794758108fd97a5dc491f1a302dd094f64
bemunin <tw.munin@gmail.com>
2024-07-29 10:45:13 +0700

    Integrate moveit2 with isaacsim successfully

--- ros_ws/src/panda_moveit_config/launch/demo.launch.py
+++ ros_ws/src/panda_moveit_config/launch/demo.launch.py
@@ -1,27 +1,33 @@
 import os
-
-from ament_index_python.packages import get_package_share_directory
-from launch_ros.actions import Node
-from moveit_configs_utils import MoveItConfigsBuilder
-
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
+from launch.conditions import IfCondition, UnlessCondition
+from launch_ros.actions import Node
+from launch.actions import ExecuteProcess
+from ament_index_python.packages import get_package_share_directory
+from moveit_configs_utils import MoveItConfigsBuilder
 
 
 def generate_launch_description():
+
     # Command-line arguments
+    tutorial_arg = DeclareLaunchArgument(
+        "rviz_tutorial", default_value="False", description="Tutorial flag"
+    )
+
+    db_arg = DeclareLaunchArgument(
+        "db", default_value="False", description="Database flag"
+    )
+
     ros2_control_hardware_type = DeclareLaunchArgument(
         "ros2_control_hardware_type",
-        default_value="isaac",
+        default_value="mock_components",
         description="ROS2 control hardware interface type to use for the launch file -- possible values: [mock_components, isaac]",
     )
 
     moveit_config = (
-        MoveItConfigsBuilder(
-            "moveit_resources_panda",
-            package_name="moveit_resources_panda_moveit_config",
-        )
+        MoveItConfigsBuilder("moveit_resources_panda")
         .robot_description(
             file_path="config/panda.urdf.xacro",
             mappings={
@@ -32,9 +38,8 @@ def generate_launch_description():
         )
         .robot_description_semantic(file_path="config/panda.srdf")
         .trajectory_execution(file_path="config/gripper_moveit_controllers.yaml")
-        .planning_pipelines(pipelines=["ompl", "pilz_industrial_motion_planner"])
-        .planning_scene_monitor(
-            publish_robot_description=True, publish_robot_description_semantic=True
+        .planning_pipelines(
+            pipelines=["ompl", "chomp", "pilz_industrial_motion_planner"]
         )
         .to_moveit_configs()
     )
@@ -49,50 +54,48 @@ def generate_launch_description():
     )
 
     # RViz
-    rviz_config_file = os.path.join(
-        get_package_share_directory("cobot"),
-        "config",
-        "panda_moveit_config.rviz",
+    tutorial_mode = LaunchConfiguration("rviz_tutorial")
+    rviz_base = os.path.join(
+        get_package_share_directory("moveit_resources_panda_moveit_config"), "launch"
     )
-
+    rviz_full_config = os.path.join(rviz_base, "moveit.rviz")
+    rviz_empty_config = os.path.join(rviz_base, "moveit_empty.rviz")
+    rviz_node_tutorial = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_empty_config],
+        parameters=[
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+            moveit_config.planning_pipelines,
+            moveit_config.robot_description_kinematics,
+        ],
+        condition=IfCondition(tutorial_mode),
+    )
     rviz_node = Node(
         package="rviz2",
         executable="rviz2",
         name="rviz2",
         output="log",
-        arguments=["-d", rviz_config_file],
+        arguments=["-d", rviz_full_config],
         parameters=[
             moveit_config.robot_description,
             moveit_config.robot_description_semantic,
-            moveit_config.robot_description_kinematics,
             moveit_config.planning_pipelines,
-            moveit_config.joint_limits,
+            moveit_config.robot_description_kinematics,
         ],
+        condition=UnlessCondition(tutorial_mode),
     )
 
     # Static TF
-    world2robot_tf_node = Node(
+    static_tf_node = Node(
         package="tf2_ros",
         executable="static_transform_publisher",
         name="static_transform_publisher",
         output="log",
-        arguments=["--frame-id", "world", "--child-frame-id", "panda_link0"],
-    )
-    hand2camera_tf_node = Node(
-        package="tf2_ros",
-        executable="static_transform_publisher",
-        name="static_transform_publisher",
-        output="log",
-        arguments=[
-            "0.04",
-            "0.0",
-            "0.04",
-            "0.0",
-            "0.0",
-            "0.0",
-            "panda_hand",
-            "sim_camera",
-        ],
+        arguments=["0.0", "0.0", "0.0", "0.0", "0.0", "0.0", "world", "panda_link0"],
     )
 
     # Publish TF
@@ -142,17 +145,34 @@ def generate_launch_description():
         arguments=["panda_hand_controller", "-c", "/controller_manager"],
     )
 
+    # Warehouse mongodb server
+    db_config = LaunchConfiguration("db")
+    mongodb_server_node = Node(
+        package="warehouse_ros_mongo",
+        executable="mongo_wrapper_ros.py",
+        parameters=[
+            {"warehouse_port": 33829},
+            {"warehouse_host": "localhost"},
+            {"warehouse_plugin": "warehouse_ros_mongo::MongoDatabaseConnection"},
+        ],
+        output="screen",
+        condition=IfCondition(db_config),
+    )
+
     return LaunchDescription(
         [
+            tutorial_arg,
+            db_arg,
             ros2_control_hardware_type,
             rviz_node,
-            world2robot_tf_node,
-            hand2camera_tf_node,
+            rviz_node_tutorial,
+            static_tf_node,
             robot_state_publisher,
             move_group_node,
             ros2_control_node,
             joint_state_broadcaster_spawner,
             panda_arm_controller_spawner,
             panda_hand_controller_spawner,
+            mongodb_server_node,
         ]
     )
