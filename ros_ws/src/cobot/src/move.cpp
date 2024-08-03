19e6eafc364fe7acaf49d2695cfaf5878f776ea3
bemunin <tw.munin@gmail.com>
2024-08-03 12:54:34 +0700

    Add forward kinematic move command pick and standby state

--- ros_ws/src/cobot/src/move.cpp
+++ ros_ws/src/cobot/src/move.cpp
@@ -3,56 +3,106 @@
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <cmath>
 
-using moveit::planning_interface::MoveGroupInterface;
+// alias
+using MoveGroupInterface = moveit::planning_interface::MoveGroupInterface;
+using Plan = moveit::planning_interface::MoveGroupInterface::Plan;
+using MoveItErrorCode = moveit::core::MoveItErrorCode;
 
+struct JointCommand {
+  std::vector<double> arm_joint_degs;
+  double gripper_pos_meter;
+};
+
+// global vars
+rclcpp::Logger const logger = rclcpp::get_logger("cobot_move");
+const int PANDA_JOINTS_NUM = 9;
+
+// utils
 double degreeToRadian(double deg){
-  return deg * M_PI / 180;
+  return deg * M_PI / 180.0;
 }
 
-auto setJointDegrees(MoveGroupInterface &movegroup, std::vector<double> joint_goal_degs){ 
-  std::vector<double> joint_goal_rads(joint_goal_degs.size());
-  for(size_t i =0; i< joint_goal_degs.size(); i++){
-    joint_goal_rads[i] = degreeToRadian(joint_goal_degs[i]);
+// helpers
+std::string cmdParser(int argc, char** argv){
+  if(argc<2){
+    return "fk:pick";
   }
-  movegroup.setJointValueTarget(joint_goal_rads);
-}
 
-auto setGripper(MoveGroupInterface &movegroup, double pos_meter){
-  std::vector<double> finger_pos_meter{pos_meter,pos_meter};
-  movegroup.setJointValueTarget(finger_pos_meter);
-}
+  std::string cmd = argv[1];
+  std::string output = "";
 
-void execute(MoveGroupInterface &movegroup, rclcpp::Logger const &logger){
-  auto const [success, plan] = [&movegroup]{
-    MoveGroupInterface::Plan msg;
-    auto const ok = static_cast<bool>(movegroup.plan(msg));
-    return std::make_pair(ok, msg);
-  }();
-
-  if(success) {
-    movegroup.execute(plan);
+  if(cmd == "ik"){
+    // TODO: Inverse Kinematic
+    return "ik:0,0,0";
   } else {
-    RCLCPP_ERROR(logger, "Planing failed!");
+    // forward kinematic, accepts state name (pick, standby)
+    output.append("fk:");
+    output.append(argv[1]);
+    return output;
+  } 
+}
+
+// Forward Kinematics
+void move_fk(MoveGroupInterface &mg, JointCommand const &target){
+  
+  std::vector<double> joints_goal;
+  joints_goal.reserve(PANDA_JOINTS_NUM);
+
+  // push arm joint value in radian
+  for(size_t i =0; i< target.arm_joint_degs.size(); i++){
+    joints_goal.push_back(degreeToRadian(target.arm_joint_degs[i]));
+  }
+
+  // push gripper 2 joint values, finger_left and right 
+  joints_goal.push_back(target.gripper_pos_meter);
+  joints_goal.push_back(target.gripper_pos_meter);
+
+  bool is_in_boundary = mg.setJointValueTarget(joints_goal);
+  if(!is_in_boundary){
+    RCLCPP_WARN(logger, "Target joints are outside of the boundary!!");
+    return;
+  }
+
+  Plan plan;
+  bool is_success = mg.plan(plan) == MoveItErrorCode::SUCCESS;
+
+  if(is_success) {
+    mg.move();
+  } else {
+    RCLCPP_ERROR(logger, "Forward kinematics planing failed!");
   }
 }
+
+// Inverse Kinematics
+void move_ik(){
+  // TODO
+}
+
+
 
 int main(int argc, char** argv)
 {
   rclcpp::init(argc, argv);
   std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("move");
-  auto const logger = rclcpp::get_logger("cobot_move");
   RCLCPP_INFO(logger, "Start node");
+  auto mg_arm = MoveGroupInterface(node, "panda_arm_hand");
 
-  auto mg_arm = MoveGroupInterface(node, "panda_arm");
+  // parse command
+  std::string cmd = cmdParser(argc, argv);
 
-  // double gripper_pos = 0.02;
-  // if(argc>1){
-  //   gripper_pos = std::stod(argv[1]);
-  // }
+  // states
+  JointCommand pick_state;
+  pick_state.arm_joint_degs = {-3,77,2,-60,3,164,38};
+  pick_state.gripper_pos_meter = 0.04;
 
-  // FK goal command
-  setJointDegrees(mg_arm,{-3,77,2,-60,3,164,38});
-  execute(mg_arm, logger);
+  JointCommand stand_by_state;
+  stand_by_state.arm_joint_degs = {0,-45,0,-135,0,92,45};
+  stand_by_state.gripper_pos_meter = 0.04;
+
+  // execute
+  if(cmd == "fk:pick") move_fk(mg_arm, pick_state);
+  else if(cmd == "fk:standby") move_fk(mg_arm, stand_by_state);
+  else move_ik();
 
   rclcpp::shutdown();
   return 0;
