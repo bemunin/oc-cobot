73d95020ae0c9461773c8dadecc30bef032018e0
bemunin <tw.munin@gmail.com>
2024-09-18 10:04:12 +0700

    Add oc cobot extension scene name argument in commandline

--- sim.py
+++ sim.py
@@ -7,6 +7,7 @@ CONFIG = {"renderer": "RayTracedLighting", "headless": False}
 sim_app = SimulationApp(CONFIG)
 
 import os
+import sys
 
 import omni
 from omni.isaac.core import World
@@ -25,17 +26,19 @@ def init_world():
     world.initialize_physics()
 
 
-def register_extensions():
+def register_extensions(ext_scene: str):
     # add custom extension path
     manager = omni.kit.app.get_app().get_extension_manager()
     oc_cobot_exts_path = f"{os.path.dirname(__file__)}/exts"
     manager.add_path(oc_cobot_exts_path)
 
+    ext_scene = ext_scene if ext_scene else "conveyor"
+
     # enable required extensions
     extensions.enable_extension("omni.kit.tool.measure")
     extensions.enable_extension("omni.isaac.conveyor")
     extensions.enable_extension("omni.isaac.ros2_bridge")
-    extensions.enable_extension("oc.cobot.conveyor")
+    extensions.enable_extension(f"oc.cobot.{ext_scene}")
 
 
 def running_sim(sim_app: SimulationApp):
@@ -53,8 +56,12 @@ def clear_world():
 
 
 if __name__ == "__main__":
+    # get omnicraft cobot extension name from command line arguments
+    args = sys.argv[1:]
+    oc_cobot_ext_scene = args[0] if args else None
+
     init_world()
-    register_extensions()
+    register_extensions(oc_cobot_ext_scene)
     running_sim(sim_app)
     clear_world()
     sim_app.close()
