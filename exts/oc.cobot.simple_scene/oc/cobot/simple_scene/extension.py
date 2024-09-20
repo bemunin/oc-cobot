5e02c42a331b3f5c8d64299d60795f68a44f95f1
bemunin <tw.munin@gmail.com>
2024-09-20 10:21:27 +0700

    Done implement oc.cobot.simple_scene moving object logic

--- exts/oc.cobot.simple_scene/oc/cobot/simple_scene/extension.py
+++ exts/oc.cobot.simple_scene/oc/cobot/simple_scene/extension.py
@@ -1,12 +1,16 @@
 import gc
 import weakref
+from collections import namedtuple
 
 import numpy as np
 import omni
 from omni.isaac.core import World
 from omni.isaac.core.prims import XFormPrim
 from omni.isaac.core.utils import stage
+from omni.isaac.core.utils.types import ArticulationAction
 from omni.isaac.franka import Franka
+from omni.isaac.franka.controllers import PickPlaceController
+from omni.isaac.franka.controllers.rmpflow_controller import RMPFlowController
 from omni.isaac.ui.menu import make_menu_item_description
 from omni.kit.menu.utils.scripts.utils import add_menu_items, remove_menu_items
 
@@ -16,9 +20,23 @@ from utils import get_proj_root_path, log
 EXTENSION_WINDOW_NAME = "Simple Scene Panel"
 
 
+Pose = namedtuple("Pose", ["position", "orientation"])
+
+
 class Extension(omni.ext.IExt):
     def on_startup(self, ext_id: str):
         self._ext_id = ext_id
+
+        # robot stage
+        self._stage = "move_cylinder"
+        self._robot = None
+        self._target_item = None
+        self._cylinder = None
+        self._cube = None
+        self._standby_joint_positions = None
+
+        # stage
+        self._count = 0
 
         # UIs & Menus
         self._window = None
@@ -40,6 +58,14 @@ class Extension(omni.ext.IExt):
             self._on_timeline_callback,
         )
 
+        if self._world.physics_callback_exists("oc.cobot.simple_scene.physics"):
+            self._world.remove_physics_callback("oc.cobot.simple_scene.physics")
+
+        self._world.add_physics_callback(
+            "oc.cobot.simple_scene.physics",
+            self._on_physics_callback,
+        )
+
     def on_shutdown(self):
         remove_menu_items(self._menu_item, self._root_menu)
         self._cleanup()
@@ -57,15 +83,50 @@ class Extension(omni.ext.IExt):
         cylinder_usd_path = get_proj_root_path() + "/assets/objects/cylinder.usd"
         stage.add_reference_to_stage(cylinder_usd_path, "/World/Cylinder")
 
+        cylinder_usd_path = get_proj_root_path() + "/assets/objects/cube.usd"
+        stage.add_reference_to_stage(cylinder_usd_path, "/World/Cube")
+
         # Add prim to scene
-        self._cylinder_prim = XFormPrim("/World/Cylinder", name="cylinder")
-        self._cylinder_prim.set_default_state(
-            position=np.array([0.8, 0, 0.08]), orientation=np.array([1, 0, 0, 0])
+        cylinder_pose = Pose(np.array([0.5, 0, 0.08]), np.array([1, 0, 0, 0]))
+        self._cylinder = XFormPrim(
+            "/World/Cylinder",
+            name="cylinder",
         )
-        self._scene.add(self._cylinder_prim)
-        self._scene.add(Franka(prim_path="/World/Robot", name="robot"))
+        self._cylinder.set_default_state(
+            position=cylinder_pose.position, orientation=cylinder_pose.orientation
+        )
+
+        cube_pose = Pose(np.array([0.5, 0.5, 0.02]), np.array([1, 0, 0, 0]))
+        self._cube = XFormPrim(
+            "/World/Cube",
+            name="cube",
+        )
+        self._cube.set_default_state(
+            position=cube_pose.position, orientation=cube_pose.orientation
+        )
+        # Add objects to scene
+        self._scene.clear(True)
+
+        self._scene.add(self._cylinder)
+        self._scene.add(self._cube)
+
+        self._robot = Franka(prim_path="/World/Robot", name="robot")
+        self._scene.add(self._robot)
 
         self._world.reset()
+
+        self._robot.gripper.set_joint_positions(
+            self._robot.gripper.joint_opened_positions
+        )
+
+        # set up robot controller
+        self._controller = PickPlaceController(
+            name="pick_place_controller",
+            gripper=self._robot.gripper,
+            robot_articulation=self._robot,
+        )
+
+        self._standby_joint_positions = self._robot.get_joint_positions().copy()
 
     def _setup_menu(self):
         # define menu items
@@ -85,7 +146,7 @@ class Extension(omni.ext.IExt):
         self._world.clear_all_callbacks()
 
     ##
-    # Handlers
+    # Handlers & Callbacks
     ##
 
     def _handle_click_menu(self):
@@ -97,4 +158,46 @@ class Extension(omni.ext.IExt):
     def _on_timeline_callback(self, event):
         if event.type == int(omni.timeline.TimelineEventType.STOP):
             log.info("Execute callback on timeline STOP")
-            self._world.reset()
+            # self._world.reset()
+
+    def _on_physics_callback(self, step_size):
+        if self._stage == "move_cylinder":
+            item_position, _ = self._cylinder.get_world_pose()
+            item_position[2] = 0.16 - 0.02
+            goal_position = np.array([0, 0.8, 0.16 - 0.02])
+            current_joint_positions = self._robot.get_joint_positions()
+
+            actions = self._controller.forward(
+                picking_position=item_position,
+                placing_position=goal_position,
+                current_joint_positions=current_joint_positions,
+            )
+            self._robot.apply_action(actions)
+
+            if self._controller.is_done():
+                self._controller.reset()
+                self._stage = "move_cube"
+        elif self._stage == "move_cube":
+            item_position, _ = self._cube.get_world_pose()
+            goal_position = np.array([0, 0.5, 0.02])
+            current_joint_positions = self._robot.get_joint_positions()
+
+            actions = self._controller.forward(
+                picking_position=item_position,
+                placing_position=goal_position,
+                current_joint_positions=current_joint_positions,
+            )
+            self._robot.apply_action(actions)
+            # Only for the pick and place controller, indicating if the state
+            # machine reached the final state.
+            if self._controller.is_done():
+                self._stage = "to_standby"
+        elif self._stage == "to_standby":
+            self._controller = RMPFlowController(
+                name="robot_rmpflow_controller", robot_articulation=self._robot
+            )
+            actions = self._controller.forward(
+                target_end_effector_position=np.array([0.39, 0, 0.45]),
+            )
+            self._robot.apply_action(actions)
+            # self._stage = "done"
