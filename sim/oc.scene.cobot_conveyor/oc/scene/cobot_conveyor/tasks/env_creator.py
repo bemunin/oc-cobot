49f2512903172ec69e68c15831100d794d512e18
bemunin <tw.munin@gmail.com>
2025-02-20 13:02:19 +0700

    Add containers to EnvCreator

--- sim/oc.scene.cobot_conveyor/oc/scene/cobot_conveyor/tasks/env_creator.py
+++ sim/oc.scene.cobot_conveyor/oc/scene/cobot_conveyor/tasks/env_creator.py
@@ -11,6 +11,8 @@ class EnvCreator(CreatorTask):
     def set_up_scene(self, scene):
         super().set_up_scene(scene)
 
+        container_usd_path = "Collected_Container_C19_61x40x18cm_PR_V_NVD_01/Container_C19_61x40x18cm_PR_V_NVD_01.usd"
+
         set_camera_view(
             eye=[1.5, -4, 5],
             target=[-0.7, 0, 0.6],
@@ -32,3 +34,29 @@ class EnvCreator(CreatorTask):
             position=np.array([-1.3, -8.9, 0.9]),
             orientation=to_quaternion([0, 0, 90]),
         )
+
+        # Container prims
+
+        containers = {
+            "container_a": (0.5, "Violet"),
+            "container_b": (0, "Blue"),
+            "container_c": (-0.5, "Gray"),
+        }
+
+        for name, attributes in containers.items():
+            y_pos = attributes[0]
+            color = attributes[1]
+            xform = self._import_obj(
+                usd_path=f"{ext_assets_path()}/{container_usd_path}",
+                prim_path=f"/World/Environment/Workspace/{name.title().replace('_', '')}",  # aka. ContainerA
+                name=name,
+                position=[-2.2, y_pos, 0.8],
+                orientation_deg=[0, 0, 0],
+                scale=[0.01, 0.01, 0.01],
+            )
+
+            color_variant = xform.prim.GetVariantSets().GetVariantSet("Color")
+            color_variant.SetVariantSelection(color)
+
+    def post_reset(self):
+        pass
