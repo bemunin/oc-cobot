3c57e9796a0359a7da36dd4ba859746dbb70c07c
bemunin <tw.munin@gmail.com>
2025-03-03 21:39:44 +0700

    Implement callback to reset /World/Execute when user stop timeline

--- sim/oc.scene.cobot_conveyor/oc/scene/cobot_conveyor/conveyor_room_sim.py
+++ sim/oc.scene.cobot_conveyor/oc/scene/cobot_conveyor/conveyor_room_sim.py
@@ -1,6 +1,9 @@
+import carb
 import numpy as np
+import omni.isaac.core.utils.prims as prims_utils
 from oc.utils.cobot.bases.base_sim import BaseSim
 from oc.utils.cobot.robots.franka_manager import FrankaManager
+from omni.timeline import TimelineEventType
 
 from .tasks.conveyor_manager import ConveyorManager
 from .tasks.env_creator import EnvCreator
@@ -20,5 +23,17 @@ class ConveyorRoomSim(BaseSim):
         robot_pos = np.array([-1.5, 0, 0.8])
         self._world.add_task(FrankaManager(name="franka", offset=robot_pos))
 
+        self._world.add_timeline_callback(
+            "oc.scene.cobot_conveyor.timeline", self._on_timeline_callback
+        )
+
     def post_reset(self):
         return
+
+    def _on_timeline_callback(self, event):
+        if event.type == int(TimelineEventType.STOP):
+            carb.log_info("ConveyorRoomSim: Timeline stopped")
+            carb.log_info("ConveyorRoomSim: Reset Execute prim group")
+
+            if prims_utils.get_prim_at_path("/World/Execute"):
+                prims_utils.delete_prim("/World/Execute")
